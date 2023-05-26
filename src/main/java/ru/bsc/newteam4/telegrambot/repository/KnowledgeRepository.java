cdd1f382b058c2bee13edda13d7331499607e2ea
aulyanov <artem.ulyanov@bsc-ideas.com>
2023-05-26 17:24:36 +0300

    addd get by id

--- src/main/java/ru/bsc/newteam4/telegrambot/repository/KnowledgeRepository.java
+++ src/main/java/ru/bsc/newteam4/telegrambot/repository/KnowledgeRepository.java
@@ -8,7 +8,7 @@ import java.util.List;
 public interface KnowledgeRepository {
 
     void loadFromMemory();
-`
+
     Knowledge getById(String id);
 
     List<Knowledge> searchByHashtag(String hashtag);
